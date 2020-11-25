811f91f06a260c681e85877bb95b70bee605d17f
Aaron Kung <CKUNG2@mail.depaul.edu>
2020-11-25 10:16:50 -0600

    Add property in CastDetailsResponseModel

--- MovieShop.Core/Models/Response_Model/CastDetailsResponseModel.cs
+++ MovieShop.Core/Models/Response_Model/CastDetailsResponseModel.cs
@@ -6,5 +6,11 @@ namespace MovieShop.Core.Models
 {
     public class CastDetailsResponseModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string TmdbUrl { get; set; }
+        public string ProfilePath { get; set; }
+        public IEnumerable<MovieResponseModel> Movies { get; set; }
     }
 }
