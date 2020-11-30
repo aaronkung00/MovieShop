ed58b724995b67ae26bc7a028a8570e692e38c82
Aaron Kung <CKUNG2@mail.depaul.edu>
2020-11-30 15:09:49 -0600

    11/30_c_Model_Binding_View_AccountReg_Req_UserRegsterRequestModel_Controller_AccountController

---
+++ MovieShop.Web/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieShop.Core.Models.Request_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShop.Web.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterRequestModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+/*
+        [HttpPost]
+        public async Task<IActionResult> Login(UserRegisterModel model)
+        {
+            return View();
+        }
+*/
+    }
+}
